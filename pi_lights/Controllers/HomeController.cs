dada637d21638c3b1a9c6cd56b99fc7abdcf2b5d
Hayden Miedema <haydenmiedema@gmail.com>
2019-08-14 21:22:42 -0400

    add first route for lights

--- pi_lights/Controllers/HomeController.cs
+++ pi_lights/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using pi_lights.Models;
+using pi_lights.Helpers;
 
 namespace pi_lights.Controllers
 {
@@ -29,7 +30,9 @@ namespace pi_lights.Controllers
         
         public ActionResult LightUp()
         {
-            return Json(new { Message = "hey" });
+            string command = "sudo PYTHON=\".:build/lib.linux-armv7l-2.7\" python /home/pi/rpi_ws281x/python/examples/strandtest.py";
+            string result = ShellHelper.Bash(command);
+            return Json(new { Message = result});
         }
     }
 }
---
+++ pi_lights/Helpers/ShellHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pi_lights.Helpers
+{
+    public class ShellHelper
+    {
+        public static string Bash(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+    }
+}
